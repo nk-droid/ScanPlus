deeaf53536034f3d7e7881d326b4f58035377018
nk <kumarnidhish22@gmail.com>
2024-05-23 20:53:02 +0530

    updated requirements.txt

--- backend/api.py
+++ backend/api.py
@@ -1,16 +1,15 @@
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app)
 api = Api(app)
 
 class UploadPrescription(Resource):
     def post(self):
         image = request.files['image']
-        print(image)
-
+        
 class Result(Resource):
     def get(self):
         pass
@@ -20,8 +19,8 @@ class AskMe(Resource):
         pass
 
 api.add_resource(UploadPrescription, '/api/upload_prescription')
-api.add_resource(AskMe, '/api/askme')
 api.add_resource(Result, '/api/result')
+api.add_resource(AskMe, '/api/askme')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
