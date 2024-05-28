a3dfff4ccd2cd17dafcd262e1d9272713759fd5c
nk <kumarnidhish22@gmail.com>
2024-05-29 04:34:09 +0530

    added modal to dashboard

--- frontend/components/dashboard.js
+++ frontend/components/dashboard.js
@@ -25,39 +25,32 @@ export const Dashboard = {
 <br>
 
 <!--med starts-->
-<div v-for="(prescriptions, key) in groupedData" :key="key">
-<div class="div12" v-for="item in prescriptions" :key="item.prescription_id">
-   
+<div class="div12" v-for="(prescriptions, key) in groupedData" :key="key">
      <img src="frontend/assets/prescription.png" height="90px" width="165" style="margin-left:-40px; margin-top:-5px;"> 
      <p style="margin-left: 100px; margin-top:-68px; color: #515b9d; font-size:15px;"> Prescription {{key}} </p>
-     <p style="margin-left: 100px; margin-top:-10px; color: #515b9d; font-size:12px;"> {{item.timestamp}} </p>
-     <button class="btn btn-secondary button-color center" style="float: right; margin-top:-50px; margin-right:20px"> View Results </button>
+     <p style="margin-left: 100px; margin-top:-10px; color: #515b9d; font-size:12px;"> {{prescriptions[0].timestamp}} </p>
+     <button class="btn btn-secondary button-color center" style="float: right; margin-top:-50px; margin-right:20px" data-bs-toggle="modal" data-bs-target="#exampleModal"> View Results </button>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+    </div>
+  </div>
 </div>
 
 <!--med ends-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          <!--end-->
-        </div> 
- </div>   <!--column end -->   
-  </div>
+  </div> 
+ </div>    
+</div>
   `,
 
   data: function() {
