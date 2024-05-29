94ba2052c2691c0c64f5615dafc7922305633878
nk <kumarnidhish22@gmail.com>
2024-05-29 07:55:31 +0530

    dumped one page

--- frontend/router.js
+++ frontend/router.js
@@ -1,7 +1,6 @@
 import { Homepage } from "./components/homepage.js";
 import { PredictResult } from "./components/predict_result.js";
 import { Predict } from "./components/predict.js";
-import { UploadPrescription } from "./components/upload_prescription.js";
 import { Ask } from "./components/ask.js";
 import { Login } from "./components/login.js";
 import { SignUp } from "./components/signup.js";
@@ -11,7 +10,6 @@ const routes = [
     { path: '/', component: Homepage, name: 'homepage'},
     { path: '/predict_result', component: PredictResult, name: 'predict_result'},
     { path: '/predict', component: Predict, name: 'predict'},
-    { path: '/upload', component: UploadPrescription, name: 'upload'},
     { path: '/ask', component: Ask, name: 'ask'},
     { path: '/login', component: Login, name: 'login'},
     { path: '/signup', component: SignUp, name: 'signup'},
